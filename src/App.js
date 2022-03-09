cc8d168fcf4d8a658384e5e27865e234796d6c74
Nate M <jeditendencies35@gmail.com>
2022-03-09 13:41:54 -0500

    didn;t add the component to the commit

--- src/App.js
+++ src/App.js
@@ -15,3 +15,6 @@ export default App;
 
 // the App return can have only 1 parent container, so wrap everything in an empty HTML fragment <></> then add whatever elements needed
 // props are essentially properties of the components, 'text' in the Header component
+// state is basically data, 2 types, component level and global
+// component level is specific to only that component, i.e. a nav container that's only opened or closed
+// global is specific to everything, an rray of data needed at multiple locations
