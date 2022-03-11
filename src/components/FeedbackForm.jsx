676711a61c14c2df4741a47512af9299f2a4fbe1
Nate M <jeditendencies35@gmail.com>
2022-03-11 17:07:22 -0500

    added typing event to form input

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import Card from './shared/Card';
 
 function FeedbackForm() {
+  const [text, setText] = useState('');
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
   return (
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
         {/* @todo - rating select component */}
         <div className="input-group">
-          <input type="text" placeholder="Write a review." />
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review."
+            value={text}
+          />
+          <button type="submit">Send</button>
         </div>
       </form>
     </Card>
@@ -15,3 +26,9 @@ function FeedbackForm() {
 }
 
 export default FeedbackForm;
+
+// typically when you have a form you have state(data) for each input in that form
+// importing useState at the top of the file is needed
+// set the state(data) as a destructured variable const [text, setText] = useState('')
+// the method/function used to update the state(data) is setText
+// onChange event is added to the input and setText is passed e.target.value to pick up key strokes
